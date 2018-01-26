30f9d9994a6033c86b5be2b265e1aa073e308f2b
Will Whitney <wfwhitney@gmail.com>
2018-01-26 18:53:53 -0500

    control the hand with a welded mocap object

--- test.py
+++ test.py
@@ -18,26 +18,46 @@ env = suite.load(domain_name="jaco", task_name="basic")
 # for domain_name, task_name in suite.BENCHMARKING:
 #   env = suite.load(domain_name, task_name)
 
-width = 320
-height = 240
-window = pyglet.window.Window(width=width, height=height, display=None)
+width = 640
+height = 480
 
+fullwidth = width * 2
+window = pyglet.window.Window(width=fullwidth, height=height, display=None)
 
 # Step through an episode and print out reward, discount and observation.
 action_spec = env.action_spec()
 time_step = env.reset()
+
+action = np.zeros([6])
+time_step = env.step(action)
+
+def move_target_to_hand():
+  env.physics.named.model.geom_pos['target'] = env.physics.named.data.xpos['jaco_link_hand']
+
+def move_mocap_to_hand():
+  env.physics.named.data.mocap_pos['weld'] = env.physics.named.data.xpos['jaco_link_hand']
+
+def zero_mocap_offset():
+  env.physics.named.model.eq_data['weld'].fill(0)
+
+zero_mocap_offset()
+
+# env.physics.named.data.mocap_pos[0] = env.physics.named.data.xpos['jaco_link_hand']
 while not time_step.last():
   # action = np.random.uniform(-1, 1, size=[6])
+  # env.physics.named.data.mocap_pos[0] = env.physics.named.data.geom_xpos['jaco_hand']
 
-  action = np.ones([6])
 
-  time_step = env.step(action)
-  # import ipdb; ipdb.set_trace()
-  # print(time_step.reward, time_step.discount, time_step.observation, env.physics.finger_to_target_distance())
-  print(time_step.reward, env.physics.finger_to_target_distance())
-  pixel = env.physics.render(height, width, camera_id=1)
-  window.clear()
-  window.switch_to()
-  window.dispatch_events()
-  pyglet.image.ImageData(width, height, 'RGB', pixel.tobytes(), pitch=width * -3).blit(0, 0)
-  window.flip()
+  for i in range(50):
+    time_step = env.step(action)
+    # print(time_step.reward, time_step.discount, time_step.observation, env.physics.finger_to_target_distance())
+    print(time_step.reward, env.physics.finger_to_target_distance())
+    pixel1 = env.physics.render(height, width, camera_id=1)
+    pixel2 = env.physics.render(height, width, camera_id=2)
+    pixel = np.concatenate([pixel1, pixel2], 1)
+    window.clear()
+    window.switch_to()
+    window.dispatch_events()
+    pyglet.image.ImageData(fullwidth, height, 'RGB', pixel.tobytes(), pitch=fullwidth * -3).blit(0, 0)
+    window.flip()
+  import ipdb; ipdb.set_trace()
